ad4d641a4342be2195692cdc3c54da19cf6ca5f6
Ajith-M-001 <ajithguru741@gmail.com>
2025-10-19 18:04:21 +0530

    04 inngest setup for background job

---
+++ src/inngest/client.ts
@@ -0,0 +1,6 @@
+//src\inngest\client.ts
+import { APP_NAME } from "@/lib/constants";
+import { Inngest } from "inngest";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: `${APP_NAME}-development` });
